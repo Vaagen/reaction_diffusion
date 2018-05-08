48ec01bdb7cdbb6acdc5cde701379ae56dfa670d
Vaagen <pcthrane@stud.ntnu.no>
2018-05-08 11:34:49 +0200

    Add c and s.

--- main.cpp
+++ main.cpp
@@ -32,6 +32,12 @@ void get_euler_explicit_matrix(double alpha, int N_grid, arma::SpMat<double>& B)
   }
 }
 
+void step_func(arma::Col<double>& c, double c0, arma::Col<double>& above_critical_c){
+  for(int i=0; i<c.n_elem; i++){
+    above_critical_c(i) = c(i) > c0;
+  }
+}
+
 void plot_vector(arma::Col<double> arma_vec, int N_grid){
   std::vector<double> vec_x(N_grid), vec_y(N_grid);
   for( int i=0; i<N_grid; i++){
@@ -53,36 +59,63 @@ int main(int argc, char *argv[]){
   double last_time=start_time;
 
   // Setup of parameters
-  double Da0 =4e-7;
+  // Tube length
+  double L   =1.0;
+  // Diffusion constants
+  double Da0 =4e-7/(L*L);
   double Db0 =2.0/3.0*Da0;
   double Dc0 =8.0/15.0*Da0;
-  double R   =1.0;
-  double L   =1.0;
+  // Reaction rate a + b -> c
+  double R   =1.0/L;
+  // Reaction rate for nucleation
+  double N1  =R/10.0;
+  // Reaction rate for deposition on solid
+  double N2  =R/10.0;
+  // Simulation time
   int T = 1000000;
 
   // Decide CFL for a, then decide delta_t from this, letting CFL for b and c be smaller.
+  // CFL 0.08 highest possible for explicit scheme for b0=10*a0
   double CFL_a = 0.08;
   // Boundary conditions
   double a0  = 1.0;
   double b0  = a0*10.0;
   // Nucleation threshold
   double c0 = 0.03;
-
+  // Grid size
   int N_grid = 1000 + 2;
+  // Parameters dependant of the ones above
   double delta_x = L/(N_grid-1);
   double delta_t = pow(delta_x,2)*CFL_a/Da0;
-
+  int T_steps = T/delta_t;
+  // To have same time step b and c must have seperate CFL numbers, we vary CFL_a since it's largest
   double CFL_b = Db0*delta_t/pow(delta_x,2);
   double CFL_c = Dc0*delta_t/pow(delta_x,2);
 
-  // Setup boundary conditions, aka initial vectors.
+  // Setup initial vectors.
   arma::Col<double> a(N_grid);
   a.fill(0);
   a(0)=a0/a0;
-
   arma::Col<double> b(N_grid);
   b.fill(0);
   b(N_grid-1)=b0/a0;
+  arma::Col<double> c(N_grid);
+  c.fill(0);
+  arma::Col<double> s(N_grid);
+  s.fill(0);
+  // To store reaction terms R*a*b
+  arma::Col<double> reaction_ab(N_grid);
+  reaction_ab.fill(0);
+  // To store reaction terms N1*c*c
+  arma::Col<double> reaction_cc(N_grid);
+  reaction_cc.fill(0);
+  // To check if c > c0
+  arma::Col<double> above_critical_c(N_grid);
+  above_critical_c.fill(0);
+  // To store reaction terms N2*c*s
+  arma::Col<double> reaction_cs(N_grid);
+  reaction_cs.fill(0);
+
 
   // Explicit euler
   // Set up matrices
@@ -90,24 +123,38 @@ int main(int argc, char *argv[]){
   get_euler_explicit_matrix(CFL_a, N_grid, B_a);
   arma::SpMat<double> B_b;
   get_euler_explicit_matrix(CFL_b, N_grid, B_b);
+  arma::SpMat<double> B_c;
+  get_euler_explicit_matrix(CFL_c, N_grid, B_c);
 
-  arma::Col<double> reaction_vec(N_grid);
-  reaction_vec.fill(0);
+  std::cout << delta_t << '\n';
 
   // Propagate time and plot.
-  for(int t=0; t<T; t++){
-    // Element wise multiplication of a and b as reaction term
-    reaction_vec = (a%b)*delta_t*R;
-    a = a - reaction_vec;
-    b = b - reaction_vec;
+  for(int t=0; t<T_steps; t++){
+    // a + b -> c
+    reaction_ab = delta_t*R*(a%b);
+    // Nucleation
+    step_func(c, c0, above_critical_c);
+    reaction_cc = delta_t*N1*(above_critical_c % (c%c));
+    // Deposition on existing solids
+    reaction_cs = delta_t*N2*(c%s);
+
+    // Add reaction terms
+    a = a - reaction_ab;
+    b = b - reaction_ab;
+    c = c + reaction_ab - reaction_cc - reaction_cs;
+    s = s               + reaction_cc + reaction_cs;
+
+    // Diffusion of gasses
     a = B_a*a;
     b = B_b*b;
+    c = B_c*c;
 
     if(t%10000==0){
       // plt::clf();
-      plot_vector(a,N_grid);
-      plot_vector(b,N_grid);
-      plot_vector(1e6*reaction_vec,N_grid);
+      // plot_vector(a,N_grid);
+      // plot_vector(b,N_grid);
+      plot_vector(s,N_grid);
+      // plot_vector(1e6*reaction_ab,N_grid);
       }
   }
 
