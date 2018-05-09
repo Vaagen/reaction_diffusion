24f0486880dae1fcc9396f6719c4595c24be532a
Vaagen <pcthrane@stud.ntnu.no>
2018-05-09 16:51:56 +0200

    For plotting subplots of densities.

--- dplot.py
+++ dplot.py
@@ -70,6 +70,7 @@ def plotMovie(a_files, b_files, c_files, s_files, filename, legend=None):
     line4, = ax.plot([], [], lw=2)
     addlabel()
     fig.legend(legend,fontsize=14,loc=0)
+    plt.tight_layout(rect=[0.01,0.01,0.915,0.95])
 
     # initialization function: plot the background of each frame
     def init():
@@ -95,25 +96,25 @@ def plotMovie(a_files, b_files, c_files, s_files, filename, legend=None):
         line4.set_data(x, s)
         addlabel()
         fig.legend(legend,fontsize=14,loc=0)
+        plt.tight_layout(rect=[0.01,0.01,0.915,0.95])
         return line1, line2, line3, line4,
 
-    # call the animator.  blit=True means only re-draw the parts that have changed.
-    anim = animation.FuncAnimation(fig, animate, init_func=init,
-                                   frames=500, interval=300, blit=True)
-    # anim.save(filename, fps=15)
+    # call the animator. blit=True only re-draw the parts that have changed.
+    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=500,
+                                    interval=100, blit=True)
+    # anim.save(filename, fps=25)
     plt.show()
 
 def addParameters(fig, ptxt):
-    fig.text(1, .1, ptxt, position=(0.85,0.2),fontsize=14)
+    fig.text(1, .1, ptxt, position=(0.85,0.1),fontsize=14)
 
-def addlabel():
+def addlabel(fig):
     plt.xlabel('X/L')
-    plt.ylabel('Density')
-
+    # plt.ylabel('Density')
 
 if __name__ == "__main__":
     # Source
-    path='output/run2/'
+    path='output/run1/'
     # Get filenames
     filenames = os.listdir(path)
     # Sort to get correct time ordering
@@ -138,9 +139,11 @@ if __name__ == "__main__":
     # Get parameters
     getVarFromFile(path + p_file)
     # Make text for parameters
-    param_names = [item for item in dir(param) if not item.startswith("__") and not item.startswith("CFL")]
+    param_names = [item for item in dir(param) if not item.startswith("__")
+                                            and not item.startswith("CFL")]
     param_vals = [getattr(param, name) for name in param_names]
-    param_text = [ param_names[i] + '= '+ '{0:.2e}'.format(param_vals[i]) for i in range(0,len(param_vals)) if not param_vals[i] == None]
+    param_text = [ param_names[i] + '= '+ '{0:.2e}'.format(param_vals[i])
+                for i in range(0,len(param_vals)) if not param_vals[i] == None]
     ptxt=''
     for i in range(0,len(param_text)):
         ptxt = ptxt + param_text[i] + '\n'
@@ -148,35 +151,85 @@ if __name__ == "__main__":
     legend = ['a', 'b', '100*c', 's']
 
     # Plotting movie
-    # plotMovie(a_files, b_files, c_files, s_files, 'diffusion_reaction_test.mp4',legend=legend)
+    # plotMovie(a_files, b_files, c_files, s_files, 'standard_params.mp4',legend=legend)
 
-    # Looking at peaks
-    # At frame frame i
-    frame = 200
+    # Plot of concentrations
+    fig1, (ax1, ax2, ax3) = plt.subplots(3, 1, sharex=True)
+
+    frame = 90
+    T1 = frame*param.f_rate*param.delta_t
     x = np.linspace(0,1,param.N_grid)
+    a = getVector(path + a_files[frame])
+    b = getVector(path + b_files[frame])
+    c = getVector(path + c_files[frame])
+    c = [100*c[i] for i in range(0,len(c))]
     s = getVector(path + s_files[frame])
-    # Finding peaks
-    peaks = peakutils.indexes(s, thres=0.1/max(s), min_dist=0.01)
-    peaknums = range(1,len(peaks))
-    peak_x = [x[j] for j in peaks]
-    peak_s = [s[j] for j in peaks]
-    fig1=plt.figure(1)
-    plt.plot(x,s)
-    plt.plot(peak_x,peak_s,'rx')
-    # Space between peaks
-    fig2=plt.figure(2)
-    delta_peak_x = np.diff(peak_x)
-    plt.plot(peaknums, list(reversed(delta_peak_x)))
-    # Time between nucleation
-    fig3 = plt.figure(3)
-    peak_t = getVector(path + t_nucleation_file)
-    peak_t = [peak_t[j] for j in peaks]
-    delta_peak_t = -np.diff(peak_t)
-    plt.plot(peaknums, list(reversed(delta_peak_t*param.delta_t)))
+    ax1.plot(x,a)
+    ax1.plot(x,b)
+    ax1.plot(x,c)
+    ax1.plot(x,s)
+
+    frame = 95
+    T = frame*param.f_rate*param.delta_t
+    x = np.linspace(0,1,param.N_grid)
+    a = getVector(path + a_files[frame])
+    b = getVector(path + b_files[frame])
+    c = getVector(path + c_files[frame])
+    c = [100*c[i] for i in range(0,len(c))]
+    s = getVector(path + s_files[frame])
+    ax2.plot(x,a)
+    ax2.plot(x,b)
+    ax2.plot(x,c)
+    ax2.plot(x,s)
+
+    frame = 100
+    T2 = frame*param.f_rate*param.delta_t
+    x = np.linspace(0,1,param.N_grid)
+    a = getVector(path + a_files[frame])
+    b = getVector(path + b_files[frame])
+    c = getVector(path + c_files[frame])
+    c = [100*c[i] for i in range(0,len(c))]
+    s = getVector(path + s_files[frame])
+    ax3.plot(x,a)
+    ax3.plot(x,b)
+    ax3.plot(x,c)
+    ax3.plot(x,s)
+
+    # ax1.set_ylabel("Density")
+    ax2.set_ylabel("Density")
+    # ax3.set_ylabel("Density")
 
     addParameters(fig1,ptxt)
-    addlabel()
-    plt.legend(legend)
+    addlabel(fig1)
+    fig1.legend(legend,loc=1)
+    plt.suptitle('T = ' + str(T1) + ' s - '+ str(T2) + ' s')
+
+    # # Looking at peaks
+    # # At frame frame i
+    # frame = 200
+    # T = frame*param.f_rate*param.delta_t
+    # x = np.linspace(0,1,param.N_grid)
+    # s = getVector(path + s_files[frame])
+    # # Finding peaks
+    # peaks = peakutils.indexes(s, thres=0.1/max(s), min_dist=0.01)
+    # peaknums = range(1,len(peaks))
+    # peak_x = [x[j] for j in peaks]
+    # peak_s = [s[j] for j in peaks]
+    # fig1=plt.figure(1)
+    # plt.plot(x,s)
+    # plt.plot(peak_x,peak_s,'rx')
+    # # Space between peaks
+    # fig2=plt.figure(2)
+    # delta_peak_x = np.diff(peak_x)
+    # plt.plot(peaknums, list(reversed(delta_peak_x)))
+    # # Time between nucleation
+    # fig3 = plt.figure(3)
+    # peak_t = getVector(path + t_nucleation_file)
+    # peak_t = [peak_t[j] for j in peaks]
+    # delta_peak_t = -np.diff(peak_t)
+    # plt.plot(peaknums, list(reversed(delta_peak_t*param.delta_t)))
+
+
 
     plt.show()
     # plt.show(block=False)
